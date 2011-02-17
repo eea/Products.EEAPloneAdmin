a7077c16ce12d75f28457a28a37548809866ab2a
tiberich <tiberiuichim@users.noreply.github.com>
2011-02-17 13:46:51 +0000

    Refs #8069; Improve code that saves resources on disk

--- Products/EEAPloneAdmin/browser/admin.py
+++ Products/EEAPloneAdmin/browser/admin.py
@@ -1,14 +1,28 @@
-from Products.Five import BrowserView
+from App.config import getConfiguration
 from Products.ATContentTypes.config import MX_TIDY_OPTIONS
 from Products.ATContentTypes.lib.validators import unwrapValueFromHTML
-try:
-    from mx.Tidy import tidy as mx_tidy
-except:
-    mx_tidy = None
-    
+from Products.CMFCore.utils import getToolByName
+from Products.Five import BrowserView
+import urllib
+import logging
+import os
 import re
 
+logger = logging.getLogger('Products.EEAPloneAdmin')
+
+try:
+    from mx.Tidy import tidy
+    has_tidy = True
+except ImportError:
+    has_tidy = False
+    
+if has_tidy:
+    mx_tidy = tidy
+else:
+    mx_tidy = lambda x,**y:x
+
 OBJNAME=  re.compile('(.*)-([A-Za-z]{2})([.][A-Za-z]{3})*$')
+
 
 class LinguaPlone(BrowserView):
     """ 
@@ -44,7 +58,8 @@ class LinguaPlone(BrowserView):
                 obj.setLanguage(lang)
                 obj.addTranslationReference(canonical)
         return [ { 'canonical' : k,
-                   'translations' : v.getTranslationLanguages() } for k,v in canonicals.items() ]
+                   'translations' : v.getTranslationLanguages() } 
+                                    for k,v in canonicals.items() ]
             
 
 class TidyContent(BrowserView):
@@ -67,3 +82,72 @@ class TidyContent(BrowserView):
                 except:
                     fixed['err'].append(obj.absolute_url())
         return fixed
+
+
+def save_resources_on_disk(registry, request):
+    """Reads merged resources from registry and saves them on disk"""
+
+    portal = getToolByName(registry, 'portal_url').getPortalObject()
+    skins = getToolByName(registry, 'portal_skins').getSkinSelections()
+    conf = getConfiguration()
+    base = conf.environment['saved_resources']
+    
+    #remove this line when we want to support multiple skins
+    skins = [skins[0]]
+
+    for skin in skins:
+        portal.changeSkin(skin) #temporarily changes current skin
+
+        #toggle these two lines when we want to support multiple skins
+        dest = base
+        #dest = os.path.join(base, urllib.quote(skin))
+
+        if not os.path.exists(dest):
+            logging.debug("%s does not exists. Creating it." % dest)
+            os.makedirs(dest)
+
+        for name in registry.concatenatedresources:
+            try:
+                content = registry.getInlineResource(name, portal)
+            except:
+                logger.warning("Could not get content for resource %s "
+                               "in skin %s" % (name, skin))
+                continue
+
+            try:
+                fpath = os.path.join(dest, name)
+                f = open(fpath, "w+")
+                f.write(content)
+                f.close()
+                logging.debug("Wrote %s on disk." % fpath)
+            except IOError:
+                logging.warning("Could not write %s on disk." % fpath)
+                pass
+
+
+class SaveResourcesOnDisk(BrowserView):
+    """Base class to save resources on disk
+    """
+    tool = None
+
+    def __call__(self):
+        save_resources_on_disk(self.tool, self.request)
+        return "Resources saved"
+
+
+class RegenerateJS(SaveResourcesOnDisk):
+    """Call this to save on disk JS resources
+    """
+
+    @property
+    def tool(self):
+        return getToolByName(self.context, "portal_javascripts")
+
+
+class RegenerateCSS(SaveResourcesOnDisk):
+    """Call this to save on disk CSS resources
+    """
+
+    @property
+    def tool(self):
+        return getToolByName(self.context, "portal_css")
--- Products/EEAPloneAdmin/event.py
+++ Products/EEAPloneAdmin/event.py
@@ -1,26 +1,4 @@
-from App.config import getConfiguration
-import os
-import logging
-
-logger = logging.getLogger('Products.EEAPloneAdmin')
-
+from Products.EEAPloneAdmin.browser.admin import save_resources_on_disk
 
 def handle_resourceregistry_change(obj, event):
-    conf = getConfiguration()
-    dest = conf.environment['saved_resources']
-    if not os.path.exists(dest):
-        os.mkdir(dest)
-    portal = obj.aq_parent
-    for name in obj.concatenatedresources:
-        try:
-            content = obj.getInlineResource(name, portal)
-        except:
-            logger.warning("Could not get content for resource %s" % name)
-            continue
-
-        try:
-            f = open(os.path.join(dest, name), "w+")
-            f.write(content)
-            f.close()
-        except IOError:
-            pass
+    save_resources_on_disk(obj)
