e9a6a07ee70ba347ee751cda03e008e056db32aa
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-05-05 12:10:30 +0000

    Refs #8291; use collective.monkeypatcher for catalog patches

--- trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_catalog.py
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_catalog.py
@@ -1,41 +1,45 @@
 from DateTime import DateTime
+from Globals import DTMLFile
+from Products.CMFCore.utils import getToolByName
 
-_enabled = []
+#from Products.CMFPlone.CatalogTool import CatalogTool
 
-def AlreadyApplied(patch):
-    if patch in _enabled:
-        return True
-    _enabled.append(patch)
-    return False
+#_enabled = []
 
-def AnonymousCatalog():
-    if AlreadyApplied('AnonymousCatalog'):
-        return
+#def AlreadyApplied(patch):
+    #if patch in _enabled:
+        #return True
+    #_enabled.append(patch)
+    #return False
 
-    from Globals import DTMLFile
-    from Products.CMFPlone.CatalogTool import CatalogTool
-    from Products.CMFCore.utils import getToolByName
+#def AnonymousCatalog():
+    #if AlreadyApplied('AnonymousCatalog'):
+        #return
 
-    def searchResults(self, REQUEST=None, **kw):
-        """ Calls ZCatalog.searchResults with extra arguments that
-            limit the results to what the user is allowed to see.
+    #CatalogTool.__eeaold_searchResults = CatalogTool.searchResults
+    #CatalogTool.searchResults = searchResults
+    #CatalogTool.__call__ = searchResults
+    #CatalogTool.manage_catalogView = DTMLFile('www/catalogView',globals())
 
-            This version adds state and effective date range for
-            anonymous users.
-        """
-        kw = kw.copy()
-        membershipTool = getToolByName(self, 'portal_membership', None)
-
-        if membershipTool.isAnonymousUser():
-            kw['review_state'] = 'published'
-            kw['effectiveRange'] = DateTime()
-                                 
-        return self.__eeaold_searchResults(REQUEST, **kw)
-
-    CatalogTool.__eeaold_searchResults = CatalogTool.searchResults
-    CatalogTool.searchResults = searchResults
-    CatalogTool.__call__ = searchResults
-    CatalogTool.manage_catalogView = DTMLFile('www/catalogView',globals())
+#AnonymousCatalog()
 
 
-AnonymousCatalog()
+view = DTMLFile('www/catalogView',globals())
+
+
+def searchResults(self, REQUEST=None, **kw):
+    """ Calls ZCatalog.searchResults with extra arguments that
+        limit the results to what the user is allowed to see.
+
+        This version adds state and effective date range for
+        anonymous users.
+    """
+    kw = kw.copy()
+    membershipTool = getToolByName(self, 'portal_membership', None)
+
+    if membershipTool.isAnonymousUser():
+        kw['review_state'] = 'published'
+        kw['effectiveRange'] = DateTime()
+                             
+    return self._old_searchResults(REQUEST, **kw)
+
