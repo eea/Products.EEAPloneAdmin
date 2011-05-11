ef85ef24ac7958f24be385fcf72b23ae4dcf65e3
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-05-11 10:58:55 +0000

    Refs #8262 and #8291; Moved workflow scripts from EEAContentTypes to EEAPloneAdmin

---
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/Extensions/CallForTender_scripts.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# File: EEAContentTypes.py
+#
+# Copyright (c) 2006 by []
+# Generator: ArchGenXML Version 1.5.1-svn
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
+__author__ = """unknown <unknown>"""
+__docformat__ = 'plaintext'
+
+
+# Workflow Scripts for: CallForTender
+
+##code-section workflow-script-header #fill in your manual code here
+##/code-section workflow-script-header
+
+
+def moveObject(self, state_change, **kw):
+    pass
+
+
