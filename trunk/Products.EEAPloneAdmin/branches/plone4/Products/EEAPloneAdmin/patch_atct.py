4088d1f156d9afab0d49df14498c0bf07caedcbe
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-07-28 10:06:11 +0000

    Fixes #8417; added patch for upstream ATCT bug

---
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_atct.py
@@ -0,0 +1,15 @@
+from Products.CMFCore.utils import getToolByName
+
+def listSubtopics(self):
+    """Return a list of our subtopics.
+    """
+    val = self.objectValues(self.meta_type)
+    check_p = getToolByName(self, 'portal_membership').checkPermission
+    tops = []
+    for top in val:
+        if check_p('View', top):
+            tops.append((top.Title().lower(),top))
+    tops.sort()
+    tops = [t[1] for t in tops]
+    return tops
+
