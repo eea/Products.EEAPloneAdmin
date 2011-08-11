2636626bea8e04a4233d205063bef12eb91733d4
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-08-11 14:20:04 +0000

    Refs #8457; added a dummy exporter for PloneSiteRoot and several other tools; solves problems when creating snapshots in portal_setup

---
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/exportimport/dummyexporter.py
@@ -0,0 +1,16 @@
+class DummyFilesystemExporter(object):
+    def __init__(self, context):
+        self.context = context
+        print "doing export for ", context
+
+    def export(self, export_context, subdir, root=False):
+        print 
+        print 
+        print 
+        print 
+        print "nothing"
+        print 
+        print 
+        print 
+        print 
+        return 
