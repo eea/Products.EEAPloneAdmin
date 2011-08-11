e79118fcd852421c200244a5cd7888cec65f7e22
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-08-11 14:53:33 +0000

    Small bugfix

--- trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_html5.py
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_html5.py
@@ -1,15 +1,14 @@
 """ Monkey patches for HTML5 integration
 """
 
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.links.viewlets import render_cachekey
 from plone.app.layout.viewlets import ViewletBase
+from plone.memoize import ram
 import plone.app.layout.links.viewlets
+
 
 class PatchedNavigationViewlet(ViewletBase):
     """ Navigation viewlet """
-
-    _template = ViewPageTemplateFile('navigation.pt')
 
     @ram.cache(render_cachekey)
     def render(self):
