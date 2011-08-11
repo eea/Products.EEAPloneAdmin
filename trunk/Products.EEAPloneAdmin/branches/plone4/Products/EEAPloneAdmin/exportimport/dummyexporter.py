af390ba433d8fc59ac38af9fccfa8325582385a6
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-08-11 14:20:52 +0000

    Refs #8457; removed debugging prints

--- trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/exportimport/dummyexporter.py
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/exportimport/dummyexporter.py
@@ -1,16 +1,6 @@
 class DummyFilesystemExporter(object):
     def __init__(self, context):
         self.context = context
-        print "doing export for ", context
 
     def export(self, export_context, subdir, root=False):
-        print 
-        print 
-        print 
-        print 
-        print "nothing"
-        print 
-        print 
-        print 
-        print 
         return 
