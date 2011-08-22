c060876b4a801aa25d5b1817a0794c3c7c82a72a
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-08-22 15:23:58 +0000

    Fixed some pylint warrnings.

--- trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_html5.py
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_html5.py
@@ -21,4 +21,4 @@ def render(self):
     """ We need to generate the link in every case, since the new url
         base detection algorithm of kss relies on it. """
     return u'<link rel="alternate" data-kss-base-url="kss-base-url" \
-             href="%s/" />' % self.context_state.object_url()
+ href="%s/" />' % self.context_state.object_url()
