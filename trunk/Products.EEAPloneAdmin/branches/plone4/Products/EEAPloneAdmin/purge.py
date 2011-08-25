8dd0986f44af7f4d0eca5fc3dbcbf59b2eb279e6
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-08-25 12:24:03 +0000

    WIP: EEA custom PURGE policy, refs #8379

---
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/purge.py
@@ -0,0 +1,31 @@
+""" Custom PURGE policy
+"""
+from z3c.caching.interfaces import IPurgePaths
+from Products.CMFCore.interfaces import IDynamicType
+
+class EEAContentPurgePaths(object):
+    """ Paths to purge for content items to include the templates defined
+        under 'Caching operations' on Site setup -> caching
+    """
+    implements(IPurgePaths)
+    adapts(IDynamicType)
+
+    def __init__(self, context):
+        """ init """
+        self.context = context
+
+    def getRelativePaths(self):
+        """ Get relative paths """
+        prefix = self.context.absolute_url_path()
+
+        self.registry = getUtility(IRegistry)
+        self.ploneSettings = self.registry.forInterface(IPloneCacheSettings)
+
+        contentTypeRulesetMapping = self.ploneSettings.contentTypeRulesetMapping
+        templateRulesetMapping = self.ploneSettings.templateRulesetMapping
+
+        yield prefix + '/my_template'
+
+    def getAbsolutePaths(self):
+        """ get absolute paths """
+        return []
