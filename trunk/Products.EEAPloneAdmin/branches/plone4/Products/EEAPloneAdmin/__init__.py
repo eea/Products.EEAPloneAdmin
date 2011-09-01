2a721801c7d6fcbedd6f4fad14f2dc4d8c8f4a0b
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-09-01 13:41:00 +0000

    Make pyflakes happy

--- trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/__init__.py
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/__init__.py
@@ -6,5 +6,5 @@ from Products.LinguaPlone import config
 config.AUTO_NOTIFY_CANONICAL_UPDATE = 0
 
 #TODO: Check why collective.monkeypatcher don't apply this patch via zcml
-import Products.EEAPloneAdmin.patch_cache
+from Products.EEAPloneAdmin import patch_cache
 __all__ = [ patch_cache.__name__ ]
