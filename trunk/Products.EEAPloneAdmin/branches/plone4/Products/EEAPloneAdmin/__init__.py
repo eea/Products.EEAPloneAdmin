350af26f9d0553aab15de48c13cc0681cc9191ec
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-09-01 10:39:17 +0000

    Fixed pylint warrnings. Fixed intendation. Wording.

--- trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/__init__.py
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/__init__.py
@@ -2,6 +2,5 @@ from Products.LinguaPlone import config
 
 config.AUTO_NOTIFY_CANONICAL_UPDATE = 0
 
-#TODO: See why collective.monkeypatcher don't apply this patch via zcml
+#TODO: Check why collective.monkeypatcher don't apply this patch via zcml
 import patch_cache
-
--- trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_cache.py
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_cache.py
@@ -7,31 +7,34 @@ from plone.app.caching.operations.utils import formatDateTime, getExpiration
 def doNotCache(published, request, response):
     """ Added extra `` no-store``, ``no-cache``, ``post-check``,
         ``pre-check`` and ``Pragma`` on headers """
-        
+
     if response.getHeader('Last-Modified'):
         del response.headers['last-modified']
 
     response.setHeader('Expires', formatDateTime(getExpiration(0)))
-    response.setHeader('Cache-Control', 'max-age=0, must-revalidate, no-store, no-cache, post-check=0, pre-check=0, private')
+    response.setHeader('Cache-Control',
+                       'max-age=0, must-revalidate, no-store, no-cache, \
+                       post-check=0, pre-check=0, private')
     response.setHeader('Pragma', 'no-cache')
-    
-plone.app.caching.operations.utils.doNotCache = doNotCache
 
-def cacheInBrowser(published, request, response, etag=None, lastModified=None):
+def cacheInBrowser(published, request, response,
+                   etag=None, lastModified=None):
     """ Added extra `` no-store``, ``no-cache``, ``post-check``,
         ``pre-check`` and ``Pragma`` on headers """
-        
+
     if etag is not None:
         response.setHeader('ETag', '"%s"' %etag, literal=1)
-        
+
     if lastModified is not None:
         response.setHeader('Last-Modified', formatDateTime(lastModified))
     elif response.getHeader('Last-Modified'):
         del response.headers['last-modified']
-    
-    response.setHeader('Expires', formatDateTime(getExpiration(0)))
-    response.setHeader('Cache-Control', 'max-age=0, must-revalidate, no-store, no-cache, post-check=0, pre-check=0, private')
-    response.setHeader('Pragma', 'no-cache')
-    
-plone.app.caching.operations.utils.cacheInBrowser = cacheInBrowser
 
+    response.setHeader('Expires', formatDateTime(getExpiration(0)))
+    response.setHeader('Cache-Control',
+                       'max-age=0, must-revalidate, no-store, no-cache, \
+                        post-check=0, pre-check=0, private')
+    response.setHeader('Pragma', 'no-cache')
+
+plone.app.caching.operations.utils.doNotCache = doNotCache
+plone.app.caching.operations.utils.cacheInBrowser = cacheInBrowser
