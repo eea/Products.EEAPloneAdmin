194dac80ee824444cdffe9fedd41e51f28a70f14
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-09-01 10:33:25 +0000

    Fixed that collective.monkeypatcher don't apply this patch, fixes #8491

--- trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/__init__.py
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/__init__.py
@@ -2,3 +2,6 @@ from Products.LinguaPlone import config
 
 config.AUTO_NOTIFY_CANONICAL_UPDATE = 0
 
+#TODO: See why collective.monkeypatcher don't apply this patch via zcml
+import patch_cache
+
--- trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_cache.py
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_cache.py
@@ -2,11 +2,12 @@
 """
 
 import plone.app.caching.operations.utils
+from plone.app.caching.operations.utils import formatDateTime, getExpiration
 
-def PatcheddoNotCache(published, request, response):
+def doNotCache(published, request, response):
     """ Added extra `` no-store``, ``no-cache``, ``post-check``,
         ``pre-check`` and ``Pragma`` on headers """
-
+        
     if response.getHeader('Last-Modified'):
         del response.headers['last-modified']
 
@@ -14,9 +15,9 @@ def PatcheddoNotCache(published, request, response):
     response.setHeader('Cache-Control', 'max-age=0, must-revalidate, no-store, no-cache, post-check=0, pre-check=0, private')
     response.setHeader('Pragma', 'no-cache')
     
-plone.app.caching.operations.utils.doNotCache = PatcheddoNotCache
+plone.app.caching.operations.utils.doNotCache = doNotCache
 
-def PatchedcacheInBrowser(published, request, response, etag=None, lastModified=None):
+def cacheInBrowser(published, request, response, etag=None, lastModified=None):
     """ Added extra `` no-store``, ``no-cache``, ``post-check``,
         ``pre-check`` and ``Pragma`` on headers """
         
@@ -32,5 +33,5 @@ def PatchedcacheInBrowser(published, request, response, etag=None, lastModified=
     response.setHeader('Cache-Control', 'max-age=0, must-revalidate, no-store, no-cache, post-check=0, pre-check=0, private')
     response.setHeader('Pragma', 'no-cache')
     
-plone.app.caching.operations.utils.cacheInBrowser = PatchedcacheInBrowser
+plone.app.caching.operations.utils.cacheInBrowser = cacheInBrowser
 
