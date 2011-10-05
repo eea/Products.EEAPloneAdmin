3171ec97ef40db7422e94de1eb604857413eba61
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-10-05 08:19:17 +0000

    Refs #8568; Fixed portal_setup->export

--- trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_genericsetup.py
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_genericsetup.py
@@ -22,3 +22,21 @@ def manage_deleteExportSteps(self, ids, request=None):
     self._p_changed = True
     url = self.absolute_url()
     request.RESPONSE.redirect("%s/manage_stepRegistry" % url)
+
+import time
+from StringIO import StringIO
+from tarfile import TarFile
+from Products.GenericSetup.context import BaseContext
+
+def __patched_init__( self, tool, encoding=None ):
+
+    BaseContext.__init__( self, tool, encoding )
+
+    timestamp = time.gmtime()
+    archive_name = ( 'setup_tool-%4d%02d%02d%02d%02d%02d.tar.gz'
+                   % timestamp[:6] )
+
+    self._archive_stream = StringIO()
+    self._archive_filename = archive_name
+    self._archive = TarFile.open( archive_name, 'w:tar'
+                                , self._archive_stream )
