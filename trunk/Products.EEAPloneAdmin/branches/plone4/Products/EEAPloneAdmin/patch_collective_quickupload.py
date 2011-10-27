54706662451878fe11bfd99f8fc7866fd7681b93
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-10-27 14:24:15 +0000

    make pylint happy

--- trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_collective_quickupload.py
+++ trunk/Products.EEAPloneAdmin/branches/plone4/Products/EEAPloneAdmin/patch_collective_quickupload.py
@@ -9,27 +9,20 @@ from collective.quickupload.browser.interfaces import IQuickUploadFileFactory
 from Products.CMFCore.utils import getToolByName
 from Acquisition import aq_inner
 
-from types import UnicodeType
 from thread import allocate_lock
 
 import transaction
 from AccessControl import Unauthorized
 from ZODB.POSException import ConflictError
 from Acquisition import aq_inner
-from zope import interface
 from zope import component
 from zope.event import notify
-from zope.lifecycleevent import ObjectModifiedEvent
 from zope.app.container.interfaces import INameChooser
 
 from plone.i18n.normalizer.interfaces import IIDNormalizer
 from Products.Archetypes.event import ObjectInitializedEvent
-from Products.CMFPlone import utils as ploneutils
-from Products.CMFCore import utils as cmfutils
 
 from collective.quickupload import logger
-from collective.quickupload.browser.interfaces import (
-    IQuickUploadCapable, IQuickUploadFileFactory)
 from Products.Archetypes.utils import shasattr
 
 upload_lock = allocate_lock()
@@ -141,7 +134,10 @@ def detect_context_rules(context, portal_type):
 
     return portal_type
 
-def QuickUploadCapableFileFactory__call__(self, name, title, description, content_type, data, portal_type):
+def QuickUploadCapableFileFactory__call__(self, name, title, description, 
+                                            content_type, data, portal_type):
+    """ Patched __call__ for QuickUploadCapableFileFactory """
+    
     context = aq_inner(self.context)
     charset = context.getCharset()
     filename = name
@@ -165,13 +161,14 @@ def QuickUploadCapableFileFactory__call__(self, name, title, description, conten
     if newid in context.objectIds() :
         # only here for flashupload method since a check_id is done
         # in standard uploader - see also XXX in quick_upload.py
-        raise NameError, 'Object id %s already exists' %newid
+        raise NameError, 'Object id %s already exists' % newid
     else :
         upload_lock.acquire()
         try:
             transaction.begin()
             try:
-                context.invokeFactory(type_name=portal_type, id=newid, title=title, description=description)
+                context.invokeFactory(type_name=portal_type, id=newid, 
+                                        title=title, description=description)
             except Unauthorized :
                 error = u'serverErrorNoPermission'
             except ConflictError :
@@ -188,15 +185,18 @@ def QuickUploadCapableFileFactory__call__(self, name, title, description, conten
                     if primaryField is not None:
                         mutator = primaryField.getMutator(obj)
                         # mimetype arg works with blob files
-                        mutator(data, content_type=content_type, mimetype=content_type)
-                        # XXX when getting file through request.BODYFILE (XHR direct upload)
-                        # the filename is not inside the file
-                        # and the filename must be a string, not unicode
-                        # otherwise Archetypes raise an error (so we use filename and not name)
+                        mutator(data, content_type=content_type, 
+                                mimetype=content_type)
+                        # XXX when getting file through request.BODYFILE 
+                        # (XHR direct upload) the filename is not inside 
+                        # the file and the filename must be a string, not 
+                        # unicode otherwise Archetypes raise an error 
+                        # (so we use filename and not name)
                         if not obj.getFilename() :
                             obj.setFilename(filename)
                             #patch:
-                            #check if setFilename worked, if not we set the filename on the blob
+                            #check if setFilename worked, 
+                            # if not we set the filename on the blob
                             if not obj.getFilename():
                                 field = obj.getPrimaryField()
                                 if field:
@@ -208,12 +208,17 @@ def QuickUploadCapableFileFactory__call__(self, name, title, description, conten
                         obj.reindexObject()
                         notify(ObjectInitializedEvent(obj))
                     else :
-                        # some products remove the 'primary' attribute on ATFile or ATImage (which is very bad)
+                        # some products remove the 'primary' attribute on 
+                        # ATFile or ATImage (which is very bad)
                         error = u'serverError'
-                        logger.info("An error happens : impossible to get the primary field for file %s, rawdata can't be created" %obj.absolute_url())
+                        logger.info("An error happens : impossible to get"\
+                            " the primary field for file %s, rawdata can't"\
+                            " be created" %obj.absolute_url())
                 else:
                     error = u'serverError'
-                    logger.info("An error happens with setId from filename, the file has been created with a bad id, can't find %s" %newid)
+                    logger.info("An error happens with setId from filename,"\
+                        " the file has been created with a bad id, can't"\
+                        " find %s" %newid)
             transaction.commit()
         finally:
             upload_lock.release()
