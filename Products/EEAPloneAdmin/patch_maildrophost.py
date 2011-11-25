628e45252d9d2d7062e5b12ecc1284a064912cd7
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2011-11-25 11:23:53 +0000

    Make EEA mailhost profile available under ZMI interface, refs #8755

--- Products/EEAPloneAdmin/patch_maildrophost.py
+++ Products/EEAPloneAdmin/patch_maildrophost.py
@@ -5,8 +5,17 @@ from Globals import package_home
 from Acquisition import aq_base
 
 EEA_CONFIG_PATH = os.path.join(package_home(globals()), 'eea_mailhost_config')
+CONFIG_PATHS = {'DEFAULT' : DEFAULT_CONFIG_PATH,
+                'EEA': EEA_CONFIG_PATH}
 
 def patched_getConfigPath(self):
     """ Get the path to the currently active configuration file
     """
     return getattr(aq_base(self), 'config_path', EEA_CONFIG_PATH)
+
+def patched_getCandidateConfigPaths(self):
+    """ Retrieve the config paths set in zope.conf
+    """
+    path_keys = CONFIG_PATHS.keys()
+    path_keys.sort()
+    return tuple([(x, CONFIG_PATHS.get(x)) for x in path_keys])
