2406ddf5f906bd8cb039cf1d45a25606b01f1168
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-11-25 11:26:25 +0000

    Fixed typo

--- trunk/Products.EEAPloneAdmin/trunk/Products/EEAPloneAdmin/patch_maildrophost.py
+++ trunk/Products.EEAPloneAdmin/trunk/Products/EEAPloneAdmin/patch_maildrophost.py
@@ -3,7 +3,7 @@
 import os
 from Globals import package_home
 from Acquisition import aq_base
-from Prodcuts.MaildropHost.MaildropHost import DEFAULT_CONFIG_PATH
+from Products.MaildropHost.MaildropHost import DEFAULT_CONFIG_PATH
 
 EEA_CONFIG_PATH = os.path.join(package_home(globals()), 'eea_mailhost_config')
 CONFIG_PATHS = {'DEFAULT' : DEFAULT_CONFIG_PATH,
