1b623ed2d8c09d41d58be7811ef92579867ef6e3
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-11-25 11:02:29 +0000

    Added EEA mailhost configuration, refs #8755

--- trunk/Products.EEAPloneAdmin/trunk/Products/EEAPloneAdmin/__init__.py
+++ trunk/Products.EEAPloneAdmin/trunk/Products/EEAPloneAdmin/__init__.py
@@ -8,3 +8,5 @@ config.AUTO_NOTIFY_CANONICAL_UPDATE = 0
 #TODO: Check why collective.monkeypatcher don't apply this patch via zcml
 from Products.EEAPloneAdmin import patch_cache
 __all__ = [ patch_cache.__name__ ]
+
+
