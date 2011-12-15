f7182973685b11d9db62464cc815941cb49b3b22
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-12-15 15:41:53 +0000

    Tagged Products.EEAPloneAdmin 4.12.

---
+++ trunk/Products.EEAPloneAdmin/tags/4.12/Products/EEAPloneAdmin/Extensions/get_oid.py
@@ -0,0 +1,25 @@
+""" Get OID
+"""
+from ZODB.utils import oid_repr
+import os
+import binascii
+
+def get_oid(self):
+    field = self.getField('image')
+    blob = field.getRaw(self).getBlob()
+    oid = blob._p_oid
+
+    directories = []
+    # Create the bushy directory structure with the least significant byte
+    # first
+    for byte in str(oid):
+        directories.append('0x%s' % binascii.hexlify(byte))
+    path = os.path.sep.join(directories)
+
+    cached = blob._p_blob_committed
+    return """<html><body>
+    oid (oid, repr, path on zeo storage): %s > %s > %s <br/>
+    cached (path to cached blob): %s <br/>
+    </body></html>
+    """ % (oid_repr(oid), oid.__repr__(), path, cached)
+
