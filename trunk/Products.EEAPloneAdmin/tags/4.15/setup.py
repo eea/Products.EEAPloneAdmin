f93feafab906242061f696cd2162b91f1186c748
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-01-17 12:40:29 +0000

    Tagged Products.EEAPloneAdmin 4.15.

---
+++ trunk/Products.EEAPloneAdmin/tags/4.15/setup.py
@@ -0,0 +1,48 @@
+""" EEA Plone Admin installer
+"""
+from setuptools import setup, find_packages
+import os
+from os.path import join
+
+name = 'Products.EEAPloneAdmin'
+path = name.split('.') + ['version.txt']
+version = open(join(*path)).read().strip()
+
+setup(name=name,
+      version=version,
+      description="EEA Plone Admin",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='eea',
+      author='European Environment Agency (EEA)',
+      author_email='webadmin@eea.europa.eu',
+      url='http://svn.eionet.europa.eu/projects/'
+          'Zope/browser/trunk/Products.EEAPloneAdmin',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.LinguaPlone',
+          'Products.PloneHelpCenter',
+          'eea.translations',
+          'Products.NavigationManager',
+          'valentine.linguaflow',
+          'collective.quickupload',
+
+          # Used in testing
+          'eea.themecentre',
+          'eea.mediacentre',
+          'eea.reports',
+      ],
+      entry_points= {
+          'console_scripts':[
+            "link_to_maildrop = Products.EEAPloneAdmin.scripts.link_maildrop:link",
+      ]},
+      )
