7870237caf61fd52022b6ec9e32bc28b55a8d2a4
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-03-22 16:09:48 +0000

    Fix plone.app.kss patch to make decorator work, refs #9002

--- trunk/Products.EEAPloneAdmin/trunk/Products/EEAPloneAdmin/patch_plone_app_kss.py
+++ trunk/Products.EEAPloneAdmin/trunk/Products/EEAPloneAdmin/patch_plone_app_kss.py
@@ -1,34 +1,50 @@
 """ Patch for plone.app.kss ver 1.7.0
 """
 import logging
-from Acquisition import aq_inner
+from Acquisition import aq_inner, Implicit
 from plone.locking.interfaces import ILockable
 from urlparse import urlsplit
 from kss.core import kssaction, KSSExplicitError
 from Products.CMFPlone import PloneMessageFactory as _
+from zope.interface import implements
+from plone.app.layout.globals.interfaces import IViewView
+from plone.app.kss.interfaces import IPloneKSSView
+from plone.app.kss.plonekssview import PloneKSSView
+from Products.CMFPlone import PloneMessageFactory as _
 
 logger = logging.getLogger('Products.EEAPloneAdmin')
 
-def changeWorkflowState(self, url):
-    context = aq_inner(self.context)
-    ksscore = self.getCommandSet('core')
-    zopecommands = self.getCommandSet('zope')
-    plonecommands = self.getCommandSet('plone')
+class ContentMenuView(Implicit, PloneKSSView):
+    """ Patch plone.app.kss ver 1.7.0, not to fail silently
+        and to display a visual progress on state change
+    """
 
-    locking = ILockable(context, None)
-    if locking is not None and not locking.can_safely_unlock():
-        selector = ksscore.getHtmlIdSelector('plone-lock-status')
-        zopecommands.refreshViewlet(selector, 'plone.abovecontent', 'plone.lockinfo')
+    implements(IPloneKSSView, IViewView)
+
+    @kssaction
+    def changeWorkflowState(self, url):
+        """ KSS Change Workflow State
+        """
+        context = aq_inner(self.context)
+        ksscore = self.getCommandSet('core')
+        zopecommands = self.getCommandSet('zope')
+        plonecommands = self.getCommandSet('plone')
+
+        locking = ILockable(context, None)
+        if locking is not None and not locking.can_safely_unlock():
+            selector = ksscore.getHtmlIdSelector('plone-lock-status')
+            zopecommands.refreshViewlet(selector, 'plone.abovecontent', 'plone.lockinfo')
+            plonecommands.refreshContentMenu()
+            return self.render()
+
+        (proto, host, path, query, anchor) = urlsplit(url)
+        if not path.endswith('content_status_modify'):
+            raise KSSExplicitError, 'content_status_modify is not handled'
+        action = query.split("workflow_action=")[-1].split('&')[0]
+        context.content_status_modify(action)
+        selector = ksscore.getCssSelector('.contentViews')
+        zopecommands.refreshViewlet(selector, 'plone.contentviews', 'plone.contentviews')
         plonecommands.refreshContentMenu()
-        return self.render()
-
-    (proto, host, path, query, anchor) = urlsplit(url)
-    if not path.endswith('content_status_modify'):
-        raise KSSExplicitError, 'content_status_modify is not handled'
-    action = query.split("workflow_action=")[-1].split('&')[0]
-    context.content_status_modify(action)
-    selector = ksscore.getCssSelector('.contentViews')
-    zopecommands.refreshViewlet(selector, 'plone.contentviews', 'plone.contentviews')
-    plonecommands.refreshContentMenu()
-    self.issueAllPortalMessages()
-    self.cancelRedirect()
+        context.plone_utils.addPortalMessage(_(u'OROAREEEEEEEEEEEEEEE'))
+        self.issueAllPortalMessages()
+        self.cancelRedirect()
