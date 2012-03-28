8832303a90fbb74ff436e7a25ebee115cda036af
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-03-28 09:29:55 +0000

    Bug fix: patched plone.app.discussion ver 2.0.10, not to fail on migrate workflows when "Discussion Item" has no workflow assigned, refs #9026

--- trunk/Products.EEAPloneAdmin/trunk/Products/EEAPloneAdmin/__init__.py
+++ trunk/Products.EEAPloneAdmin/trunk/Products/EEAPloneAdmin/__init__.py
@@ -5,6 +5,8 @@ from Products.LinguaPlone import config
 
 config.AUTO_NOTIFY_CANONICAL_UPDATE = 0
 
-#TODO: Check why collective.monkeypatcher don't apply this patch via zcml
+#TODO: Check why collective.monkeypatcher don't apply patches below via zcml
 from Products.EEAPloneAdmin import patch_cache
-__all__ = [ patch_cache.__name__ ]
+from Products.EEAPloneAdmin import patch_plone_app_discussion
+__all__ = [ patch_cache.__name__,
+            patch_plone_app_discussion.__name__ ]
