8ae19554988ef5973a23e0540488103d1983f07d
tiberich <tiberiuichim@users.noreply.github.com>
2012-04-20 13:00:18 +0000

    Refs #8387; added subscriber to update creationdate on copy/paste of tree

--- Products/EEAPloneAdmin/event.py
+++ Products/EEAPloneAdmin/event.py
@@ -1,6 +1,10 @@
 """ Event
 """
 from Products.EEAPloneAdmin.browser.admin import save_resources_on_disk
+from DateTime import DateTime
 
 def handle_resourceregistry_change(obj, event):
     save_resources_on_disk(obj)
+
+def handle_object_copied(obj, event):
+    obj.creation_date = DateTime()
