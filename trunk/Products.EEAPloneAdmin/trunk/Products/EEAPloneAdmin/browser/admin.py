524f834ad1643e8efa68b38fe269d7b13290d7e2
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-07-26 09:36:24 +0000

    Refs #9331; fix saving of resources on disk

--- trunk/Products.EEAPloneAdmin/trunk/Products/EEAPloneAdmin/browser/admin.py
+++ trunk/Products.EEAPloneAdmin/trunk/Products/EEAPloneAdmin/browser/admin.py
@@ -134,48 +134,57 @@ def save_resources_on_disk(registry, request=None):
     script          = conf.environment.get('sync_resources')
     default_url     = conf.environment.get('portal_url', portal_url)
 
-    for skin in skins:
-        portal.changeSkin(skin) #temporarily changes current skin
 
-        #toggle these two lines when we want to support multiple skins
-        #dest = base   #only one skin
+    for skin in skins:
+        if skin not in ['EEADesign2006', 'EEADesignCMS']:
+            continue
+        #portal.changeSkin(skin) #temporarily changes current skin
+
         dest = os.path.join(base, skin)
 
         if not os.path.exists(dest):
             logging.debug("%s does not exists. Creating it." % dest)
             os.makedirs(dest)
 
-        if not getattr(registry, 'concatenatedresources', None):
+        if not getattr(registry, 'concatenatedResourcesByTheme', None):
             logging.warning("No concatenated resources in registry")
             continue
 
-        for name in registry.concatenatedresources:
-            try:
-                content = getResourceContent(registry, name, registry)
-            except KeyError:
-                #this is for DTML base_properties problem
-                logging.warning("Could not generate content "
-                                "for %s in skin %s" % (name, skin))
-                continue
+        for theme, resources in registry.concatenatedResourcesByTheme.items():
+            for name in resources:
+                try:
+                    #content = getResourceContent(registry, name, registry)
+                    content = registry.getResourceContent(name, 
+                                                context=registry, theme=skin)
+                except TypeError:
+                    continue    #old merged resource
+                except (KeyError, AttributeError, AssertionError), e:
+                    if not str(e).strip():   #on empty error, content is saved
+                        continue
+                    #this is for DTML base_properties problem
+                    logging.warning("Could not generate content "
+                                    "for %s in skin %s because: %s" % 
+                                        (name, skin, e))
+                    continue
 
-            if isinstance(content, str):
-                content = content.decode('utf-8', 'ignore')
+                if isinstance(content, str):
+                    content = content.decode('utf-8', 'ignore')
 
-            content = localize(content, default_url, portal_url)
+                content = localize(content, default_url, portal_url)
 
-            try:
-                fpath = os.path.join(dest, name)
-                parent = os.path.dirname(fpath)
-                if not os.path.exists(parent):
-                    logging.debug("%s does not exists. Creating it." % dest)
-                    os.makedirs(parent)
+                try:
+                    fpath = os.path.join(dest, name)
+                    parent = os.path.dirname(fpath)
+                    if not os.path.exists(parent):
+                        logging.debug("%s does not exists. Creating it." % dest)
+                        os.makedirs(parent)
 
-                f = codecs.open(fpath, 'w', 'utf-8')
-                f.write(content)
-                f.close()
-                logging.debug("Wrote %s on disk." % fpath)
-            except IOError:
-                logging.warning("Could not write %s on disk." % fpath)
+                    f = codecs.open(fpath, 'w', 'utf-8')
+                    f.write(content)
+                    f.close()
+                    logging.debug("Wrote %s on disk." % fpath)
+                except IOError:
+                    logging.warning("Could not write %s on disk." % fpath)
 
     if script:
         res = subprocess.call([script, base])
@@ -183,145 +192,6 @@ def save_resources_on_disk(registry, request=None):
             raise ValueError("Unsuccessful synchronisation of disk resources")
 
     logger.info(u"Finished saving resources on disk for registry %s" % registry)
-
-
-def getResourceContent(registry, item, context, original=False):
-    """Fetch resource content for delivery."""
-
-    self = registry
-
-    # Save the RESPONSE headers
-    headers = self.REQUEST.RESPONSE.headers.copy()
-
-    ids = self.concatenatedresources.get(item, None)
-    resources = self.getResourcesDict()
-    if ids is not None:
-        ids = ids[:]
-    output = u""
-    if len(ids) > 1:
-        output = output + self.merged_output_prefix
-
-    portal = getToolByName(context, 'portal_url').getPortalObject()
-
-    if context == self and portal is not None:
-        context = portal
-
-    default_charset = 'utf-8'
-
-    for res_id in ids:
-        try:
-            if portal is not None:
-                obj = context.restrictedTraverse(res_id)
-            else:
-                #Can't do anything other than attempt a getattr
-                obj = getattr(context, res_id)
-        except (AttributeError, KeyError):
-            output += u"\n/* ERROR -- could not find '%s'*/\n" % res_id
-            content = u''
-            obj = None
-        except Unauthorized:
-            #If we're just returning a single resource, raise an Unauthorized,
-            #otherwise we're merging resources in which case just log an error
-            if len(ids) > 1:
-                #Object probably isn't published yet
-                output += (
-                u"\n/* ERROR -- access to '%s' not authorized */\n" % res_id)
-                content = u''
-                obj = None
-            else:
-                raise
-
-        if obj is not None:
-            if isinstance(obj, z3_Resource):
-                content = get_resource_content(obj, self, default_charset)
-            elif hasattr(aq_base(obj),'meta_type') and  obj.meta_type in \
-                ['DTML Method', 'Filesystem DTML Method']:
-                content = obj(client=self.aq_parent, REQUEST=self.REQUEST,
-                              RESPONSE=self.REQUEST.RESPONSE)
-                contenttype = self.REQUEST.RESPONSE.headers.get('content-type',
-                        '')
-                contenttype = getCharsetFromContentType(contenttype,
-                        default_charset)
-                content = unicode(content, contenttype)
-            elif hasattr(aq_base(obj),'meta_type') and \
-                    obj.meta_type == 'Filesystem File':
-                obj._updateFromFS()
-                content = obj._readFile(0)
-                contenttype = getCharsetFromContentType(obj.content_type,
-                        default_charset)
-                content = unicode(content, contenttype)
-            elif hasattr(aq_base(obj),'meta_type') and obj.meta_type in \
-                    ('ATFile', 'ATBlob'):
-                f = obj.getFile()
-                contenttype = getCharsetFromContentType(f.getContentType(),
-                        default_charset)
-                content = unicode(str(f), contenttype)
-            # We should add more explicit type-matching checks
-            elif hasattr(aq_base(obj), 'index_html') and \
-                    callable(obj.index_html):
-                #self._removeCachingHeaders()
-                content = obj.index_html(self.REQUEST,
-                                         self.REQUEST.RESPONSE)
-                if not isinstance(content, unicode):
-                    content = unicode(content, default_charset)
-            elif callable(obj):
-                try:
-                    content = obj(self.REQUEST, self.REQUEST.RESPONSE)
-                except TypeError:
-                    # Could be a view or browser resource
-                    content = obj()
-                except AttributeError, err:
-                    logger.exception(err)
-                    content = u''
-
-                if IStreamIterator.providedBy(content):
-                    content = content.read()
-
-                if content and not isinstance(content, unicode):
-                    content = unicode(content, default_charset)
-            else:
-                content = str(obj)
-                content = unicode(content, default_charset)
-
-        # Add start/end notes to the resource for better
-        # understanding and debugging
-        if content:
-            output += u'\n/* - %s - */\n' % (res_id,)
-            if original:
-                output += content
-            else:
-                output += self.finalizeContent(resources[res_id], content)
-            output += u'\n'
-
-    # File objects and other might manipulate the headers,
-    # something we don't want. we set the saved headers back
-    self.REQUEST.RESPONSE.headers = headers
-    return output
-
-
-def get_resource_content(obj, registry, default_charset):
-    """ Get resource content
-    """
-    self = registry
-    try:
-        method = obj.__browser_default__(self.REQUEST)[1][0]
-    except AttributeError: # zope.app.publisher.browser.fileresource
-        try:
-            method = obj.browserDefault(self.REQUEST)[1][0]
-        except (AttributeError, IndexError):
-            try:
-                method = obj.browserDefault(self.REQUEST)[0].__name__
-            except AttributeError:
-                method = 'GET'
-    method = method in ('HEAD','POST') and 'GET' or method
-    content = getattr(obj, method)()
-
-    if not isinstance(content, unicode):
-        contenttype = self.REQUEST.RESPONSE.headers.get('content-type', '')
-        contenttype = getCharsetFromContentType(contenttype, default_charset)
-        content = unicode(content, contenttype)
-
-    return content
 
 
 class SaveResourcesOnDisk(BrowserView):
