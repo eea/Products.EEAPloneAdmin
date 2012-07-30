ee729a35f8a6e3796772aa302a38459fec2cc6b5
ichim-david <ichim.david@gmail.com>
2012-07-30 14:50:55 +0000

    Fixes #6299; monkeypatch Five.browser.BrowserView to add a HEAD method just like page templates

--- Products/EEAPloneAdmin/patch_atctmixin.py
+++ Products/EEAPloneAdmin/patch_atctmixin.py
@@ -5,35 +5,10 @@
  for all zope3 views.
 """
 
-from zExceptions import NotFound, MethodNotAllowed
-from Acquisition import aq_base
 from webdav.Resource import Resource as WebdavResoure
-from zope.component.interfaces import ComponentLookupError
-from zope.component import getMultiAdapter
+
 
 def HEAD(self, REQUEST, RESPONSE):
-    """Overwrite HEAD method for HTTP HEAD requests
-
-    Returns 404 Not Found if the default view can't be acquired or 405
-    Method not allowed if the default view has no HEAD method.
-
-    Added proxy HTTP HEAD requests for zope3 views.
+    """ Patch Five.BrowserView to have a HEAD method just like other resources
     """
-    view_id = self.getDefaultPage() or self.getLayout()
-    view_method = getattr(self, view_id, None)
-    if view_method is None:
-        try:
-            # check if any zope3 view as default page
-            z3view = getMultiAdapter((self, REQUEST), name=view_id)
-            return WebdavResoure.HEAD(z3view.context, REQUEST, RESPONSE)
-        except ComponentLookupError:
-            # view method couldn't be acquired
-            raise NotFound, "View method %s for requested resource is not " \
-                             "available." % view_id
-    if getattr(aq_base(view_method), 'HEAD', None) is not None:
-        # view method has a HEAD method
-        return view_method.__of__(self).HEAD(REQUEST, RESPONSE)
-    else:
-        raise MethodNotAllowed, 'Method not supported for this resource.'
-
-
+    return WebdavResoure.HEAD(self.context, REQUEST, RESPONSE)
