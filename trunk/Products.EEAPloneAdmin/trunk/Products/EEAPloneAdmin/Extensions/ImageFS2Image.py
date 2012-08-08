de725db4bd61d50e6b180ea189cfc0da680de059
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-08-08 07:49:16 +0000

    Refs #8174; WIP migrate ImageFS 2 Image

--- trunk/Products.EEAPloneAdmin/trunk/Products/EEAPloneAdmin/Extensions/ImageFS2Image.py
+++ trunk/Products.EEAPloneAdmin/trunk/Products/EEAPloneAdmin/Extensions/ImageFS2Image.py
@@ -4,14 +4,45 @@ from plone.app.blob.migrations import ATImageToBlobImageMigrator
 from zope.component import getUtility
 from Products.CMFCore.interfaces import ISiteRoot
 #from StringIO import StringIO
-from plone.app.blob.migrations import migrate
+#from plone.app.blob.migrations import migrate
+from Products.CMFCore.utils import getToolByName
+from Products.Archetypes.interfaces import IReferenceable
+
+import logging
+logger = logging.getLogger('Products.EEAPloneAdmin')
 
 def ImageFS2Image(self):
     """ Migrate ImageFS to Image"""
     portal = getUtility(ISiteRoot)
-    migrate(portal, 'ImageFS')
+
+    ctool = getToolByName(portal, 'portal_catalog')
+#    brains = ctool.unrestrictedSearchResults(portal_type='ImageFS')
+    brains = ctool.unrestrictedSearchResults(Title='img16')
+    total = len(brains)
+    logger.info(('Migrating %s instances of '
+                 'ImageFS to Image'), total)
+
+    for index, brain in enumerate(brains[0:5]):
+        logger.info('\t Migration status: %s/%s', index+1, total)
+        try:
+            img = brain.getObject()
+            migrator = imageMigrator(img)
+            migrator.migrate()
+            migrator.new.reindexObject()
+        except Exception, err:
+            logger.exception(err)
+
+    logger.info(('Finish migration of %s instances of '
+                 'ImageFS to Image'), total)
+
 
 class imageMigrator(ATImageToBlobImageMigrator):
     """ Migrator """
-    src_portal_type = 'ImageFS'
-    src_meta_type = 'ImageFS'
+    def migrate_at_uuid(self):
+        """ migrate at uuid """
+        if not IReferenceable.providedBy(self.old):
+            return  # old object doesn't support AT uuids
+        uid = self.old.UID()
+        self.old._uncatalogUID(self.parent)
+#        import pdb; pdb.set_trace()
+        self.new._setUID(uid)
