e52a3e37eac8613be20487887fea6460428d9e90
demarant <demarant@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-08-28 13:59:22 +0000

    Tagged Products.EEAPloneAdmin 5.2.

---
+++ trunk/Products.EEAPloneAdmin/tags/5.2/Products/EEAPloneAdmin/__init__.py
@@ -0,0 +1,15 @@
+""" Init
+"""
+
+from Products.LinguaPlone import config
+
+config.AUTO_NOTIFY_CANONICAL_UPDATE = 0
+
+#ZZZ: Check why collective.monkeypatcher don't apply patches below via zcml
+from Products.EEAPloneAdmin import patch_cache
+from Products.EEAPloneAdmin import patch_plone_app_discussion
+from Products.EEAPloneAdmin import patch_statusmessages
+
+__all__ = [ patch_cache.__name__,
+            patch_plone_app_discussion.__name__,
+            patch_statusmessages.__name__ ]
