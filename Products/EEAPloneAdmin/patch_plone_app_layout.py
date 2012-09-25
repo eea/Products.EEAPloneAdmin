a88b8e53f8c77d00e69ab8c09960f0f2d51a18ff
ichim-david <ichim.david@gmail.com>
2012-09-25 12:32:02 +0000

    Refs #9518; added patch for getNavigationRoot to set the root of the site to the site + context language

---
+++ Products/EEAPloneAdmin/patch_plone_app_layout.py
@@ -0,0 +1,59 @@
+""" plone.app.layout patches
+"""
+
+from plone.app.layout.navigation.root import getNavigationRootObject
+from Products.CMFCore.utils import getToolByName
+
+
+def getNavigationRoot(context, relativeRoot=None):
+    """Get the path to the root of the navigation tree.
+
+    If a relativeRoot argument is provided, navigation root is computed from
+    portal path and this relativeRoot.
+
+    If no relativeRoot argument is provided, and there is a root value set in
+    portal_properties, navigation root path is computed from
+    portal path and this root value.
+
+    IMPORTANT !!!
+    Previous paragraphs imply relativeRoot is relative to the Plone portal.
+
+    Else, a root must be computed : loop from the context to the portal,
+    through parents, looking for an object implementing INavigationRoot.
+    Return the path of that root.
+    """
+    portal_url = getToolByName(context, 'portal_url')
+
+    if relativeRoot is None:
+        # fetch from portal_properties
+        portal_properties = getToolByName(context, 'portal_properties')
+        navtree_properties = getattr(portal_properties, 'navtree_properties')
+        relativeRoot = navtree_properties.getProperty('root', None)
+    
+    ### original
+    # if relativeRoot has a meaningful value,
+    #if relativeRoot and relativeRoot != '/':
+    #    # use it
+    #
+    #    # while taking care of case where
+    #    # relativeRoot is not starting with a '/'
+    #    if relativeRoot[0] != '/':
+    #        relativeRoot = '/' + relativeRoot
+
+    #    portalPath = portal_url.getPortalPath()
+    #    return portalPath + relativeRoot
+
+    ### patch
+    if relativeRoot:
+        portalPath = portal_url.getPortalPath()
+        lang = context.Language()
+        # set relativeRoot to be the language of the context
+        relativeRoot = relativeRoot + lang if lang != 'en' else '/SITE'
+        return portalPath + relativeRoot
+    ### end patch
+    
+    else:
+        # compute the root
+        portal = portal_url.getPortalObject()
+        root = getNavigationRootObject(context, portal)
+        return '/'.join(root.getPhysicalPath())
