2154214d1f6e6ba5c5fb9b4d18e19c098989e2d1
ichim-david <ichim.david@gmail.com>
2013-08-05 10:12:43 +0000

    Refs #15234; modified utils script to do migration of p4a.* in order to do this in the same time that we wake up all the objects in the site

--- Products/EEAPloneAdmin/upgrades/utils.py
+++ Products/EEAPloneAdmin/upgrades/utils.py
@@ -2,6 +2,9 @@
 """
 import logging
 import transaction
+from zope.component import ComponentLookupError
+from zope.component.interface import nameToInterface
+from zope.interface import noLongerProvides
 
 
 logger = logging.getLogger("Products.EEAPloneAdmin.upgrades")
@@ -9,15 +12,35 @@ info = logger.info
 info_exception = logger.exception
 
 
-def bulkReindexObjects(context, brains, idxs=[]):
+def bulkReindexObjects(context, brains, idxs=None):
     """ Bulk reindex objects using multi-transactions """
     total = len(brains)
     info('INFO: Start reindexing')
     info('INFO: reindexing %s brains', total)
+    inames = [
+        "p4a.subtyper.interfaces.ISubtyped",
+        "p4a.video.interfaces.IAnyVideoCapable",
+        "p4a.video.interfaces.IPossibleVideoContainer",
+        "p4a.video.interfaces.IPossibleVideo",
+        "p4a.plonevideoembed.interfaces.IAnyVideoLinkCapable",
+    ]
+    ifaces = []
+    for iname in inames:
+        try:
+             ifaces.append(nameToInterface(context, iname))
+        except ComponentLookupError:
+            info_exception('Cant find interface from %s name', iname)
+
+    if idxs and type(idxs) == list:
+        idxs.append('object_provides')
+
     for index, brain in enumerate(brains):
         try:
             obj = brain.getObject()
             if idxs:
+                for iface in ifaces:
+                    if iface.providedBy(obj):
+                        noLongerProvides(obj, iface)
                 obj.reindexObject(idxs=idxs)
             else:
                 obj.reindexObject()
