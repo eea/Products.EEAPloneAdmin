dc7d9131d119002e8308321f75189fdc091d59b3
ichim-david <ichim.david@gmail.com>
2013-08-09 12:42:32 +0000

    * Feature: added ability to disable saving of resources on disk by adding a   boolean property named *disableResourceDiskSaving* inside site_properties   useful when performing site upgrades and you want to avoid having the   resources saved every time an upgrade step cooks the resources   [ichimdav refs #14323]

--- Products/EEAPloneAdmin/event.py
+++ Products/EEAPloneAdmin/event.py
@@ -1,13 +1,17 @@
 """ Event
 """
+from Products.CMFCore.utils import getToolByName
 from Products.EEAPloneAdmin.browser.admin import save_resources_on_disk
 from DateTime import DateTime
 
 def handle_resourceregistry_change(obj, event):
     """ Handle resource registry modification
     """
-    if getattr(event, 'force', False):
-        save_resources_on_disk(obj)
+    portal_properties = getToolByName(obj, 'portal_properties')
+    site_properties = portal_properties.get('site_properties')
+    if not site_properties.getProperty('disableResourceDiskSaving'):
+        if getattr(event, 'force', False):
+            save_resources_on_disk(obj)
 
 def handle_object_copied(obj, event):
     """ Handle object copy/paste
