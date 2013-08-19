dee223cb8753cd1cdec8f84cab4c29c05bc6d0c0
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-08-19 13:28:56 +0000

    Tagged Products.EEAPloneAdmin 7.2.

---
+++ trunk/Products.EEAPloneAdmin/tags/7.2/Products/EEAPloneAdmin/event.py
@@ -0,0 +1,25 @@
+""" Event
+"""
+from Products.CMFCore.utils import getToolByName
+from Products.EEAPloneAdmin.browser.admin import save_resources_on_disk
+from DateTime import DateTime
+
+def handle_resourceregistry_change(obj, event):
+    """ Handle resource registry modification
+    """
+    portal_properties = getToolByName(obj, 'portal_properties')
+
+    site_properties = portal_properties.get('site_properties')
+    if not site_properties:
+        if getattr(event, 'force', False):
+            save_resources_on_disk(obj)
+        return
+
+    if not site_properties.getProperty('disableResourceDiskSaving'):
+        if getattr(event, 'force', False):
+            save_resources_on_disk(obj)
+
+def handle_object_copied(obj, event):
+    """ Handle object copy/paste
+    """
+    obj.creation_date = DateTime()
