48da68ec9565d3ba77fc275a96e2a42c64be050b
tiberich <tiberiuichim@users.noreply.github.com>
2013-09-17 07:51:26 +0000

    Refs #16746; upload script as it has been used in migration

--- Products/EEAPloneAdmin/Extensions/fix_versionids.py
+++ Products/EEAPloneAdmin/Extensions/fix_versionids.py
@@ -4,23 +4,29 @@ import logging
 
 logger = logging.getLogger("EEAPloneAdmin.versionfix")
 
-def fix_versionids(context):
-    catalog = context.portal_catalog
+def fix_versionids(self):
+    catalog = self.portal_catalog
     brains = catalog.searchResults(missing=True, Language="all")
 
     i = 0
     for brain in brains:
-        if not brain.getVersionId:
-            obj = brain.getObject()
-            assign_new_version_id(obj, event=None)
-            obj.reindexObject(idxs=['getVersionId'])
-            logger.debug("Fixed %s", brain.getURL())
+        try:
+            if not brain.getVersionId:
+                obj = brain.getObject()
+                assign_new_version_id(obj, event=None)
+                obj.reindexObject(idxs=['getVersionId'])
+                #print "reindexed", brain.getURL()
+                #logger.debug("Fixed %s", brain.getURL())
 
-        i += 1
-        if i % 100 == 0:
-            transaction.savepoint()
-            logger.info("Savepoint at %s", i)
+            i += 1
+            if i % 100 == 0:
+                transaction.commit()
+                print "savepoint at ", i
+                #logger.info("Savepoint at %s", i)
+        except:
+            print "Exception on ", brain.getURL()
 
-    logger.info("Done fixing versionids")
+    #logger.info("Done fixing versionids")
+    print "done"
     return "Done"
 
