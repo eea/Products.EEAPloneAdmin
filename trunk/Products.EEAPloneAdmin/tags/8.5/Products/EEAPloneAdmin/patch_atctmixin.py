eee454638a17dc58aaf8a1d9d6e0d99ec014cd2b
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2014-02-13 15:51:40 +0000

    Tagged Products.EEAPloneAdmin 8.5.

---
+++ trunk/Products.EEAPloneAdmin/tags/8.5/Products/EEAPloneAdmin/patch_atctmixin.py
@@ -0,0 +1,12 @@
+""" MONKEY PATCH
+ this should be fixed in ATContentTypes to take in consideration
+ the case of zope3 view as default page. Also a fix should be
+ done for Five.BrowserView in order to handle HEAD requests
+ for all zope3 views.
+"""
+from webdav.Resource import Resource as WebdavResoure
+
+def HEAD(self, REQUEST, RESPONSE):
+    """ Patch Five.BrowserView to have a HEAD method just like other resources
+    """
+    return WebdavResoure.HEAD(self.context, REQUEST, RESPONSE)
