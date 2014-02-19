23dbdaed04e878d05e3f7a9a207f61bd0f35b852
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2014-02-19 17:58:07 +0000

    * Feature: patched Products.CMFCore deleted members not to endup in database   conflict due to the long process of deletion   [ghicaale refs #18138]

---
+++ trunk/Products.EEAPloneAdmin/trunk/Products/EEAPloneAdmin/patch_cmfcore.py
@@ -0,0 +1,38 @@
+""" Patch Products.CMFCore due to #18138, as deleting a local user
+    is a very long process which most usually endup in database conflict error
+    on a live portal
+"""
+
+from Products.CMFCore.utils import _checkPermission
+from Products.CMFCore.permissions import ChangeLocalRoles
+from AccessControl.requestmethod import postonly
+from Acquisition import aq_base
+import transaction
+import logging
+
+logger = logging.getLogger("Products.EEAPloneAdmin.patch_cmfcore")
+
+def patched_deleteLocalRoles(self, obj, member_ids, reindex=1, recursive=0,
+                             REQUEST=None, object_count=0):
+        """ Delete local roles of specified members.
+        """
+        object_count = object_count + 1
+        if object_count % 10000 == 0:
+            transaction.commit()
+            logger.info('Deleting members:')
+            logger.info(member_ids)
+
+        if _checkPermission(ChangeLocalRoles, obj):
+            for member_id in member_ids:
+                if obj.get_local_roles_for_userid(userid=member_id):
+                    obj.manage_delLocalRoles(userids=member_ids)
+                    break
+
+        if recursive and hasattr( aq_base(obj), 'contentValues' ):
+            for subobj in obj.contentValues():
+                self.deleteLocalRoles(subobj, member_ids, 0, 1, REQUEST,
+                                      object_count)
+
+        if reindex and hasattr(aq_base(obj), 'reindexObjectSecurity'):
+            # reindexObjectSecurity is always recursive
+            obj.reindexObjectSecurity()
