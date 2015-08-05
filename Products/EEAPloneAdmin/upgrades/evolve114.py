e638e1fe8b8143059e2d3a094470319b44d172e4
Alin Voinea <contact@avoinea.com>
2015-08-05 07:48:43 +0000

    Refs #27760 - Better logging

--- Products/EEAPloneAdmin/upgrades/evolve114.py
+++ Products/EEAPloneAdmin/upgrades/evolve114.py
@@ -1,8 +1,17 @@
 """ Removing 'zc.async' left over from old single database configuration
 """
+import logging
 import transaction
-
+logger = logging.getLogger('Products.EEAPloneAdmin')
 
 def remove_old_async(context):
-	context._p_jar.root().pop('zc.async')
-	transaction.commit()
+    """ Cleanup old zc.async queue from Data.fs
+    """
+    logger.info('Removing zc.async from Data.fs')
+    queue = context._p_jar.root().pop('zc.async', None)
+    if queue is None:
+        logger.info("Nothing to do - zc.async not found within Data.fs.")
+        return
+
+    logger.info("Found 1 zc.async. Removing it...")
+    transaction.commit()
