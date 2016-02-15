c017bf52edd18800ef664a77ac62158b21f025e3
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2016-02-15 12:28:17 +0000

    Tagged Products.EEAPloneAdmin 12.4.

---
+++ trunk/Products.EEAPloneAdmin/tags/12.4/Products/EEAPloneAdmin/event.py
@@ -0,0 +1,70 @@
+""" Event
+"""
+from Products.CMFCore.utils import getToolByName
+from Products.EEAPloneAdmin.browser.admin import save_resources_on_disk
+from DateTime import DateTime
+
+def handle_resourceregistry_change(obj, event):
+    """ Handle resource registry modification
+    """
+    portal_properties = getToolByName(obj, 'portal_properties')
+
+    site_properties = portal_properties.get('site_properties')
+    if not site_properties:
+        if getattr(event, 'force', False):
+            save_resources_on_disk(obj)
+        return
+
+    if not site_properties.getProperty('disableResourceDiskSaving'):
+        if getattr(event, 'force', False):
+            save_resources_on_disk(obj)
+
+
+def handle_object_copied(obj, event):
+    """ Handle object copy/paste
+    """
+    obj.creation_date = DateTime()
+
+
+def handle_object_cloned(obj, event):
+    """ Handle object pasted within the final destination
+    """
+    ptype = obj.portal_type
+    if ptype == "CallForInterest" or ptype == "CallForTender":
+        return
+    if obj.effective_date:
+        obj.setEffectiveDate('None')
+
+
+def handle_workflow_change(obj, event):
+    """ Handle object workflow change and remove effectiveDate
+        if the review_state is no longer published
+    """
+    # 20827 remove effective date from object if review_state is
+    # no longer published.
+    # This event is triggered also when there is an object clone
+    # after the object copied event and before the object is cloned
+    # skip changes for CallForInterest since the open and close
+    # date properties of the object is populating the expiration
+    # and publishing date
+    ptype = obj.portal_type
+    if ptype == "CallForInterest" or ptype == "CallForTender":
+        return
+    review_state = event.status['review_state']
+    # set effectiveDate to that of the EEAFigure for all EEAFigureFiles
+    # when EEAFigure is published 20827
+    if review_state == "published":
+        parent_date = obj.effective_date
+        if ptype == "EEAFigure":
+            query = {'portal_type': 'EEAFigureFile'}
+            cur_path = '/'.join(obj.getPhysicalPath())
+            query['path'] = {'query': cur_path, 'depth': 1}
+            figbrains = obj.portal_catalog(query)
+            for brain in figbrains:
+                figure = brain.getObject()
+                figure.setEffectiveDate(parent_date)
+    else:
+        if obj.effective_date:
+            obj.setEffectiveDate('None')
+
+
---
+++ trunk/Products.EEAPloneAdmin/tags/12.4/Products/EEAPloneAdmin/quickupload.py
@@ -0,0 +1,54 @@
+""" Quick Upload
+"""
+from zope.component import adapts
+from zope.interface import implements
+
+from Products.Archetypes.interfaces import IBaseObject
+
+from collective.quickupload.interfaces import IQuickUploadFileSetter
+from collective.quickupload import logger
+from Products.Archetypes.utils import shasattr
+
+
+class ArchetypesFileSetter(object):
+    """ File Setter
+    """
+    implements(IQuickUploadFileSetter)
+    adapts(IBaseObject)
+
+    def __init__(self, context):
+        self.context = context
+
+    def set(self, data, filename, content_type):
+        """ Set
+        """
+        error = ''
+        obj = self.context
+        primaryField = obj.getPrimaryField()
+        if primaryField is not None:
+            mutator = primaryField.getMutator(obj)
+            # mimetype arg works with blob files
+            mutator(data, content_type=content_type, mimetype=content_type)
+            if not obj.getFilename():
+                obj.setFilename(filename)
+                #patch:
+                #check if setFilename worked,
+                # if not we set the filename on the blob
+                if not obj.getFilename():
+                    field = obj.getPrimaryField()
+                    if field:
+                        blob = field.getUnwrapped(obj)
+                        if blob is not None:
+                            if shasattr(blob, 'setFilename'):
+                                blob.setFilename(filename)
+                #end of patch
+            obj.reindexObject()
+        else:
+            # some products remove the 'primary' attribute
+            # on ATFile or ATImage (which is very bad)
+            error = u'serverError'
+            logger.info("An error happens : impossible to get the primary field"
+                        " for file %s, rawdata can't be created",
+                        obj.absolute_url())
+
+        return error
