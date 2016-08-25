7806211b55ce9b4ccbb961a853bd49cea71e482f
DragosChirila <dragos.chirila@eaudeweb.ro>
2016-08-25 10:23:22 +0200

    * Bug fix: Patched Products.LinguaPlone getDefaultPage to overcome   security assessments during traversal   [chiridra refs #74776]

--- Products/EEAPloneAdmin/patch_linguaplone.py
+++ Products/EEAPloneAdmin/patch_linguaplone.py
@@ -2,6 +2,9 @@
 """
 from archetypes.schemaextender import extender
 
+from Products.CMFCore.utils import getToolByName
+from Products.LinguaPlone.browser.defaultpage import DefaultPage
+
 def clearSchemaCache(context):
     """ Clear
     """
@@ -14,3 +17,19 @@ def _patched_getFieldsToCopy(self, *args, **kwargs):
     """
     clearSchemaCache(self.context)
     return self._old_getFieldsToCopy(*args, **kwargs)
+
+
+def _patched_getDefaultPage(self):
+    """ Get the translation of the folder default page in current language
+    """
+    default_page = super(DefaultPage, self).getDefaultPage()
+    if not default_page:
+        return default_page
+
+    page = self.context.unrestrictedTraverse([default_page])
+    languageTool = getToolByName(self.context, 'portal_languages')
+    current = languageTool.getPreferredLanguage()
+    if page.hasTranslation(current):
+        return page.getTranslation(current).getId()
+    else:
+        return default_page
