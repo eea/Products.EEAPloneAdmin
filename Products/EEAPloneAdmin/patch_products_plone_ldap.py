fa97ebda3dcaefb53d075cb3711ce8dc48cabf16
ichim-david <ichim.david@gmail.com>
2016-12-08 17:02:15 +0200

    Refs #79711 revert pylint fix leaving argument named id

--- Products/EEAPloneAdmin/patch_products_plone_ldap.py
+++ Products/EEAPloneAdmin/patch_products_plone_ldap.py
@@ -5,8 +5,8 @@ import logging
 
 logger = logging.getLogger("patch_products_plone_ldap")
 
-def __patched_init__(self, oid, user):
-    self.id = oid
+def __patched_init__(self, id, user):
+    self.id = id
 
     acl = self._getLDAPUserFolder(user)
     self._ldapschema = [(x['ldap_name'], x['public_name'],
@@ -22,5 +22,5 @@ def __patched_init__(self, oid, user):
     if isinstance(properties, tuple):
         logger.warning(properties)
         properties = properties[0] if properties else {}
-    UserPropertySheet.__init__(self, oid,
+    UserPropertySheet.__init__(self, id,
             schema=[(x[1], x[2]) for x in self._ldapschema], **properties)
