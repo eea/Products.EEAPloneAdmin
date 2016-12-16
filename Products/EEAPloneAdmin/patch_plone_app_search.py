d1660e02e2699f6809fd89efe848b07140e2cf13
ichim-david <ichim.david@gmail.com>
2016-12-16 16:20:06 +0200

    Refs #79924 fix sort_on CatalogError
    - Either when using values with non alphanumeric characters or
      when using an index name that is not part of the list which
      you can select from the search page

--- Products/EEAPloneAdmin/patch_plone_app_search.py
+++ Products/EEAPloneAdmin/patch_plone_app_search.py
@@ -2,46 +2,51 @@
     of non alphanumeric characters
 """
 from Products.CMFCore.utils import getToolByName
-from Products.Five import BrowserView
 from Products.ZCTextIndex.ParseTree import ParseError
 from Products.CMFPlone.PloneBatch import Batch
 from plone.app.contentlisting.interfaces import IContentListing
 import re
 
 
-class Search(BrowserView):
+def results(self, query=None, batch=True, b_size=10, b_start=0):
+    """ Get properly wrapped search results from the catalog.
+    Everything in Plone that performs searches should go through this view.
+    'query' should be a dictionary of catalog parameters.
+    """
+    if query is None:
+        query = {}
+    if batch:
+        query['b_start'] = b_start = int(b_start)
+        query['b_size'] = b_size
+    # 79924 patch str values in order to avoid bad values
+    # in case it contains non alphabetic characters
+    p = re.compile(r"[^A-Za-z0-9-_]+")
+    query = self.filter_query(query)
+    for k, v in query.items():
+        if k == 'SearchableText':
+            continue
+        if not isinstance(v, basestring):
+            continue
+        if p.search(v):
+            query[k] = re.sub(p, "", v)
+        if k == 'sort_on':
+            # 79924
+            # you should only be allowed to search by date or sortable_title
+            # by removing the sort_on key if we have other values passed in
+            # we avoid a CatalogError
+            if v and v not in ['Date', 'sortable_title']:
+                del query[k]
 
-    valid_keys = ('sort_on', 'sort_order', 'sort_limit', 'fq', 'fl', 'facet')
+    if query is None:
+        res = []
+    else:
+        catalog = getToolByName(self.context, 'portal_catalog')
+        try:
+            res = catalog(**query)
+        except ParseError:
+            return []
 
-    def results(self, query=None, batch=True, b_size=10, b_start=0):
-        """ Get properly wrapped search results from the catalog.
-        Everything in Plone that performs searches should go through this view.
-        'query' should be a dictionary of catalog parameters.
-        """
-        if query is None:
-            query = {}
-        if batch:
-            query['b_start'] = b_start = int(b_start)
-            query['b_size'] = b_size
-        # 79924 patch values in order to avoid bad values
-        p = re.compile(r"[^A-Za-z0-9-_]+")
-        for k, v in query.items():
-            if k == 'SearchableText':
-                continue
-            if p.match(v):
-                query[k] = re.sub(p, v)
-        query = self.filter_query(query)
-
-        if query is None:
-            results = []
-        else:
-            catalog = getToolByName(self.context, 'portal_catalog')
-            try:
-                results = catalog(**query)
-            except ParseError:
-                return []
-
-        results = IContentListing(results)
-        if batch:
-            results = Batch(results, b_size, b_start)
-        return results
+    res = IContentListing(res)
+    if batch:
+        res = Batch(res, b_size, b_start)
+    return res
