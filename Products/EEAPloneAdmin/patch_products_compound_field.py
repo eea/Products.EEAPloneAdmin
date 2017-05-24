0e271ead6f5b99c6b82a9aef5a99f98e6491ea85
David Ichim <ichim.david@gmail.com>
2017-05-24 18:20:44 +0200

    Refs #84611 removed logging and fixed as many pylints as possible from the original code

--- Products/EEAPloneAdmin/patch_products_compound_field.py
+++ Products/EEAPloneAdmin/patch_products_compound_field.py
@@ -2,13 +2,13 @@
 """
 import types
 from Products.CompoundField import config
+
 ListTypes = (types.TupleType, types.ListType)
-import logging
-
-log = logging.getLogger()
 
 
-def set(self, instance, value, **kwargs):
+def patched_set(self, instance, value, **kwargs):
+    """ plone patch
+    """
     if not value:
         return
 
@@ -16,7 +16,6 @@ def set(self, instance, value, **kwargs):
     # disabled by default
     if config.EVIL_EVAL and type(value) in types.StringTypes:
         #if the value comes as string eval it to a dict
-        # XXX attention: use restricted environment instead!
         # this is a potential security hole.
         value = eval(value)
 
@@ -27,22 +26,22 @@ def set(self, instance, value, **kwargs):
     for f in self.Schema().fields():
         if value.has_key(f.old_name):
             v = value[f.old_name]
-            isarray = type(v) in ListTypes and len(v)==2 and type(v[1]) == types.DictType
+            isarray = type(v) in ListTypes and len(v) == 2 and \
+                type(v[1]) == types.DictType
             if v and isarray:
-                kw=v[1]
+                kw = v[1]
             else:
-                kw={}
+                kw = {}
 
             request = instance.REQUEST
-            if (v or \
-                f.type == 'lines' and \
+            if v or f.type == 'lines' and \
                 not ('controller_state' in request and \
-                     request['controller_state'].getErrors())):
-                if isarray or (type(v) in ListTypes and len(v) ==1) and f.type != 'datagrid':
+                     request['controller_state'].getErrors()):
+                if isarray or (type(v) in ListTypes and len(v) == 1) and \
+                    f.type != 'datagrid':
                     f.set(instance, v[0], **kw)
                 else:
                     f.set(instance, v, **kw)
             #### eea patch allow setting of empty values if we have a datetime
-            log.info('called it david')
             if not v and f.type == 'datetime':
                 f.set(instance, v, **kw)
