88d9f2ca1e87004ac34ed64a1ec11e4ef9f18c7d
Alec Ghica <alecghica@gmail.com>
2017-07-10 12:58:26 +0200

    updated/adjusted script to delete all enquiries
    * Change: updated/adjusted script to delete all enquiries and requestors
      [alecghica refs #86470]

--- Products/EEAPloneAdmin/Extensions/Enquiry_scripts.py
+++ Products/EEAPloneAdmin/Extensions/Enquiry_scripts.py
@@ -43,29 +43,61 @@ def deleteEnquiryDatabase(self):
     portal = api.portal.get()
     enquiries = portal['SITE']['help']['infocentre']['enquiries']
     requestors = portal['SITE']['help']['infocentre']['enquiries']['requestors']
+    requestors2 = portal['SITE']['help']['infocentre']['enquiries']['enquiry-requestor-folder']
 
     enquiries_total = len(enquiries.keys())
     logger.info("%s enquiries found.", enquiries_total)
     requestors_total = len(requestors.keys())
     logger.info("%s requestors found.", requestors_total)
+    requestors_total2 = len(requestors2.keys())
+    logger.info("%s requestors2 found.", requestors_total2)
+    total_count = 0
+    total_count_break = 1000000
 
     # delete requestors
     logger.info("Starting to delete requestors!")
     count = 0
-    for k in requestors.keys():
+    count_to_commit = 20
+    to_delete = [ x for x in requestors.keys()]
+    for k in to_delete:
         requestors.manage_delObjects(ids=[k])
         count += 1
-        if count % 100 == 0:
+        logger.info("Deleted: %s / %s", count, count_to_commit)
+        total_count += 1
+        if total_count == total_count_break:
+            break
+        if count % count_to_commit == 0:
             logger.info("Requestors deleted: %s / %s", count, requestors_total)
+            transaction.commit()
+
+    # delete requestors2
+    logger.info("Starting to delete requestors2!")
+    count = 0
+    count_to_commit = 20
+    to_delete = [ x for x in requestors2.keys()]
+    for k in to_delete:
+        requestors2.manage_delObjects(ids=[k])
+        count += 1
+        logger.info("Deleted: %s / %s", count, count_to_commit)
+        total_count += 1
+        if total_count == total_count_break:
+            break
+        if count % count_to_commit == 0:
+            logger.info("Requestors deleted: %s / %s", count, requestors_total2)
             transaction.commit()
 
     # delete enquiries
     logger.info("Starting to delete enquiries!")
     count = 0
-    for k in enquiries.keys():
+    to_delete = [x for x in enquiries.keys()]
+    for k in to_delete:
         enquiries.manage_delObjects(ids=[k])
         count += 1
-        if count % 100 == 0:
+        logger.info("Deleted: %s / %s", count, count_to_commit)
+        total_count += 1
+        if total_count == total_count_break:
+            break
+        if count % count_to_commit == 0:
             logger.info("Enquiries deleted: %s / %s", count, enquiries_total)
             transaction.commit()
 
