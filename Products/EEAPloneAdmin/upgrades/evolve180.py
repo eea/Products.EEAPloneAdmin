c7acc586c70a921de9fb28983602d697c467f40d
Alin Voinea <alin.voinea@eaudeweb.ro>
2018-05-25 12:24:25 +0300

    Refs #95561 - Add upgrade step to cleanup traceview

---
+++ Products/EEAPloneAdmin/upgrades/evolve180.py
@@ -0,0 +1,20 @@
+""" Cleanup Zope Versions Control
+"""
+import logging
+from zope.component import getUtility
+from plone.app.viewletmanager.interfaces import IViewletSettingsStorage
+
+logger = logging.getLogger("Products.EEAPloneAdmin")
+
+def cleanup_traceview(context):
+    """ Cleanup traceview from viewletmanager
+    """
+    storage = getUtility(IViewletSettingsStorage)
+    for skin, managers in storage._order.items():
+        for manager, order in managers.items():
+            if order.startswith(u"traceview"):
+                logger.warn(
+                    "Cleanup traceview from viewlet storage: %s",
+                    managers.pop(manager)
+                )
+
