a6d2dfb2d1bba9818f55ec99078c21b333d2d4bc
Alin Voinea <alin.voinea@eaudeweb.ro>
2018-05-25 12:41:16 +0300

    Refs #95561 - Cleanup viewlet managers

--- Products/EEAPloneAdmin/upgrades/evolve180.py
+++ Products/EEAPloneAdmin/upgrades/evolve180.py
@@ -13,8 +13,17 @@ def cleanup_traceview(context):
     for skin, managers in storage._order.items():
         for manager, order in managers.items():
             if 'traceview.top' in order or 'traceview.buttom' in order:
-                logger.warn(
-                    "Cleanup traceview from viewlet storage: %s",
-                    managers.pop(manager)
-                )
+                new_order = tuple(v for v in order if v not in (
+                                  u"traceview.top", u"traceview.buttom"))
+                if not new_order:
+                    logger.warn(
+                        "Cleanup traceview from viewlet storage: %s = %s",
+                        manager, managers.pop(manager)
+                    )
+                else:
+                    logger.warn(
+                        "Cleanup traceview from viewlet storage: %s = %s",
+                        manager, order
+                    )
+                    managers[manager] = new_order
 
