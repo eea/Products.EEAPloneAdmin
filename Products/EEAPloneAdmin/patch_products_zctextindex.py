f56e4e911349f5426f194363ab72a7ad3ca4ab26
Petchesi-Iulian <iamnotacat12@gmail.com>
2020-03-19 12:10:25 +0100

    Refs #111954 Added patch for Products.ZCTextIndex so that it will continue deleting object even if the index returns a keyerror for one object.

---
+++ Products/EEAPloneAdmin/patch_products_zctextindex.py
@@ -0,0 +1,22 @@
+""" Patch Products.ZCTextIndex ver >= 2.13.5:
+        - unindex doc;
+"""
+import logging
+from Products.ZCTextIndex.BaseIndex import unique
+
+logger = logging.getLogger('Prodcuts.EEAPloneAdmin')
+
+def unindex_doc(self, docid):
+    for wid in unique(self.get_words(docid)):
+        try:
+            self._del_wordinfo(wid, docid)
+        except:
+            logger.error("Failed to delete index entry with wid: %s and docid: %s" % (wid, docid))
+            continue
+    del self._docwords[docid]
+    del self._docweight[docid]
+    try:
+        self.document_count.change(-1)
+    except AttributeError:
+        # Upgrade document_count to Length object
+        self.document_count = Length(self.document_count())
