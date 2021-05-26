df914e7e0a3b0eac8e0036480a97de89ea788f5c
David Ichim <ichim.david@gmail.com>
2021-05-26 20:51:58 +0300

    Refs #134252 changed the following:
    - moved patches to patches module, cleaning up
      package root of patch files now that we have so many
    - added patch_plone_app_contentrules_handlers.py patch which ensures
      that the content rules engine goes beyond SITE as parent with ISiteRoot

--- Products/EEAPloneAdmin/__init__.py
+++ Products/EEAPloneAdmin/__init__.py
@@ -9,23 +9,21 @@ from Products.LinguaPlone import config
 # Patch for plone.app.caching ver 1.0 to add extra headers
 # Patch for plone.app.caching ver 1.1.8 to proper check method names
 #    - patch for the 1.1.8 was merged to core under version > 1.1.8
-from Products.EEAPloneAdmin import patch_plone_app_caching
 
 # Patch plone.app.discussion ver >= 2.0.10, not to fail on migrate
 # workflows when "Discussion Item" has no workflow assigned
-from Products.EEAPloneAdmin import patch_plone_app_discussion
 
 # Patch plone.app.layout ver 2.2.7, due to #9518
-from Products.EEAPloneAdmin import patch_plone_app_layout
+from Products.EEAPloneAdmin.patches import patch_plone_app_layout, \
+    patch_plone_app_caching, patch_plone_session, patch_plone_app_folder, \
+    patch_plone_app_discussion
 
 # Patch plone.session ver 3.5.2, due to #13992
 # To be removed once plone.session ver > 3.5.3 (patch submitted to Plone core)
-from Products.EEAPloneAdmin import patch_plone_session
 from Products.EEAPloneAdmin import translation_negotiator
 
 # Patch plone.app.folder ver 1.1.3 due to #120304
 # we have a folder "themes" that has an illegal ID name as a property name
-from Products.EEAPloneAdmin import patch_plone_app_folder
 
 config.AUTO_NOTIFY_CANONICAL_UPDATE = 0
 
